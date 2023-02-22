bddfd9c43de05f331c9a4ec5576b6a58b7e61c70
rupesh-72 <120365138+rupesh-72@users.noreply.github.com>
2023-02-22 21:23:56 +0530

    Time: 8 ms (57.11%), Space: 10.2 MB (57.13%) - LeetHub

--- 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.cpp
+++ 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.cpp
@@ -1,23 +1,46 @@
+// class Solution {
+// public:
+//     vector<int> intersection(vector<int>& nums1, vector<int>& nums2) {
+//         vector<int> res;
+        
+//         sort(nums1.begin(),nums1.end()); 
+//         auto it = unique(nums1.begin(), nums1.end()); // returns iterator of last element
+//         nums1.resize(distance(nums1.begin(), it)); // distance returns number of elements and                                                       resize function resizes the vector size
+        
+//         for(int i = 0; i < nums1.size(); i++)
+//         {
+//             for(int j = 0; j < nums2.size(); j++)
+//             {
+//                 if(nums1[i] == nums2[j])
+//                 {
+//                     res.push_back(nums1[i]);
+//                     break;
+//                 }
+//             }
+//         }
+//         return res;
+//     }
+// };
+
+
+// Optimized using unordered_set
 class Solution {
 public:
     vector<int> intersection(vector<int>& nums1, vector<int>& nums2) {
         vector<int> res;
-        
-        sort(nums1.begin(),nums1.end()); 
-        auto it = unique(nums1.begin(), nums1.end()); // returns iterator of last element
-        nums1.resize(distance(nums1.begin(), it)); // distance returns number of elements and                                                       resize function resizes the vector size
-        
+        unordered_set<int> s;
         for(int i = 0; i < nums1.size(); i++)
+            s.insert(nums1[i]);
+        
+        for(int i = 0; i < nums2.size(); i++)
         {
-            for(int j = 0; j < nums2.size(); j++)
+            if(s.find(nums2[i]) != s.end())
             {
-                if(nums1[i] == nums2[j])
-                {
-                    res.push_back(nums1[i]);
-                    break;
-                }
+                res.push_back(nums2[i]);
+                s.erase(nums2[i]);
             }
         }
         return res;
     }
 };
+
