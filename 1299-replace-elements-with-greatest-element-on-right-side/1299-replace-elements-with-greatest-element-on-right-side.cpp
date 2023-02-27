c801073b928144a4d5f78be4c2d333ccb6f3f4d9
rupesh-72 <120365138+rupesh-72@users.noreply.github.com>
2023-02-27 21:17:03 +0530

    Time: 133 ms (34.10%), Space: 68.6 MB (43.74%) - LeetHub

--- 1299-replace-elements-with-greatest-element-on-right-side/1299-replace-elements-with-greatest-element-on-right-side.cpp
+++ 1299-replace-elements-with-greatest-element-on-right-side/1299-replace-elements-with-greatest-element-on-right-side.cpp
@@ -1,15 +1,34 @@
+// class Solution {
+// public:
+//     vector<int> replaceElements(vector<int>& arr) {
+//         vector<int> res;
+//         int maxVal;
+//         for(int i = 1; i < arr.size(); i++)
+//         {
+//             maxVal = *max_element(arr.begin() + i, arr.end());
+//             res.push_back(maxVal);
+//         }
+//         res.push_back(-1);
+        
+//         return res;
+//     }
+// };
+
+
+// Optimized Solution in O(n)
+
 class Solution {
 public:
     vector<int> replaceElements(vector<int>& arr) {
-        vector<int> res;
-        int maxVal;
-        for(int i = 1; i < arr.size(); i++)
-        {
-            maxVal = *max_element(arr.begin() + i, arr.end());
-            res.push_back(maxVal);
-        }
-        res.push_back(-1);
+        int n = arr.size();
+        vector<int> res(n);
         
+        res[n-1] = -1;
+        
+        for(int i = n-2; i >= 0; i--)
+        {
+            res[i] = max(res[i+1], arr[i+1]);
+        }
         return res;
     }
 };
