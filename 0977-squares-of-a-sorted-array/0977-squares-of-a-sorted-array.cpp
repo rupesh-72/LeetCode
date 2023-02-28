6914bb53c735afce42f14f447f74b624032b93d5
rupesh-72 <120365138+rupesh-72@users.noreply.github.com>
2023-02-28 22:22:54 +0530

    Time: 27 ms (95.10%), Space: 25.9 MB (59.14%) - LeetHub

--- 0977-squares-of-a-sorted-array/0977-squares-of-a-sorted-array.cpp
+++ 0977-squares-of-a-sorted-array/0977-squares-of-a-sorted-array.cpp
@@ -1,14 +1,40 @@
+// class Solution {
+// public:
+//     vector<int> sortedSquares(vector<int>& nums) {
+//         vector<int> res;
+//         int val;
+//         for(int i = 0; i < nums.size(); i++)
+//         {
+//             val = nums[i] * nums[i];
+//             res.push_back(val);
+//         }
+//         sort(res.begin(), res.end());
+//         return res;
+//     }
+// };
+
+// Optimized Approach O(n)
+
 class Solution {
 public:
     vector<int> sortedSquares(vector<int>& nums) {
-        vector<int> res;
-        int val;
-        for(int i = 0; i < nums.size(); i++)
+        vector<int> res(nums.size());
+        int l = 0;
+        int r = nums.size() - 1;
+        
+        for(int i = nums.size() - 1; i >= 0; i--)
         {
-            val = nums[i] * nums[i];
-            res.push_back(val);
+            if(abs(nums[r]) > abs(nums[l]))
+            {
+                res[i] = nums[r] * nums[r];
+                r--;
+            }
+            else
+            {
+                res[i] = nums[l] * nums[l];
+                l++;
+            }
         }
-        sort(res.begin(), res.end());
         return res;
     }
 };
