62a7e531cb3c58726afaa7901b4e817c04d634bd
rupesh-72 <120365138+rupesh-72@users.noreply.github.com>
2023-03-12 16:34:05 +0530

    Time: 8 ms (72.60%), Space: 9.6 MB (43.61%) - LeetHub

--- 0137-single-number-ii/0137-single-number-ii.cpp
+++ 0137-single-number-ii/0137-single-number-ii.cpp
@@ -1,14 +1,36 @@
+// class Solution {
+// public:
+//     int singleNumber(vector<int>& nums) {
+//         unordered_map<int,int> mp;
+//         for(int i = 0; i < nums.size(); i++)
+//             mp[nums[i]]++;
+//         for(int i = 0; i < nums.size(); i++)
+//         {
+//             if(mp[nums[i]] == 1)
+//                 return nums[i];
+//         }
+//         return 0;
+//     }
+// };
+
+// optimized approach using Bit Manipulation
+// Tutorial - https://www.youtube.com/watch?v=Nt920vgqXyE
 class Solution {
 public:
     int singleNumber(vector<int>& nums) {
-        unordered_map<int,int> mp;
-        for(int i = 0; i < nums.size(); i++)
-            mp[nums[i]]++;
-        for(int i = 0; i < nums.size(); i++)
-        {
-            if(mp[nums[i]] == 1)
-                return nums[i];
+        int ans = 0;
+        for(int i = 0; i < 32; i++)
+        {   
+            int count1 = 0;
+            for(int j = 0; j < nums.size(); j++)
+            {
+                if((1 << i) & nums[j])
+                    count1++;
+            }
+            
+            if(count1 % 3 == 1)
+                ans = ans | (1 << i);
         }
-        return 0;
+        return ans;
     }
 };
