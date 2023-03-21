4362c635a64747ce5c8f1fc86da9b35f1020f315
rupesh-72 <120365138+rupesh-72@users.noreply.github.com>
2023-03-21 23:10:13 +0530

    Time: 5 ms (35.45%), Space: 6.8 MB (57.43%) - LeetHub

--- 0884-uncommon-words-from-two-sentences/0884-uncommon-words-from-two-sentences.cpp
+++ 0884-uncommon-words-from-two-sentences/0884-uncommon-words-from-two-sentences.cpp
@@ -1,17 +1,58 @@
+// class Solution {
+// public:
+//     vector<string> uncommonFromSentences(string s1, string s2) {
+//         unordered_map<string, int> mp;
+//         vector<string> res;
+        
+//         istringstream str1(s1);
+//         string word;
+//         while(str1 >> word) 
+//             mp[word]++;
+        
+//         istringstream str2(s2);
+//         while(str2 >> word) 
+//             mp[word]++;
+        
+//         for(auto it = mp.begin(); it != mp.end(); it++)
+//         {
+//             if(it->second == 1)
+//                 res.push_back(it->first);
+//         }
+//         return res;
+//     }
+// };
+
+// Optimized Approach
 class Solution {
 public:
     vector<string> uncommonFromSentences(string s1, string s2) {
         unordered_map<string, int> mp;
         vector<string> res;
-        
-        istringstream str1(s1);
-        string word;
-        while(str1 >> word) 
-            mp[word]++;
-        
-        istringstream str2(s2);
-        while(str2 >> word) 
-            mp[word]++;
+        string temp;
+        for(auto ch : s1)
+        {
+            if(ch != ' ')
+                temp.push_back(ch);
+            else
+            {
+                mp[temp]++;
+                temp = "";
+            }
+        }
+        mp[temp]++; // inserting last word of string
+        temp = ""; // empty temp
+               
+        for(auto ch : s2)
+        {
+            if(ch != ' ')
+                temp.push_back(ch);
+            else
+            {
+                mp[temp]++;
+                temp = "";
+            }
+        }
+        mp[temp]++; // inserting last word of string
         
         for(auto it = mp.begin(); it != mp.end(); it++)
         {
