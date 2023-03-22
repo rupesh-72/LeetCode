727fdbcc2ee28d765327c6d0994388ce01a2531d
rupesh-72 <120365138+rupesh-72@users.noreply.github.com>
2023-03-22 21:07:39 +0530

    Time: 8 ms (99.54%), Space: 18 MB (25.44%) - LeetHub

--- 0268-missing-number/0268-missing-number.cpp
+++ 0268-missing-number/0268-missing-number.cpp
@@ -1,14 +1,30 @@
+// class Solution {
+// public:
+//     int missingNumber(vector<int>& nums) {
+//         unordered_set<int> s;
+//         for(int i = 0; i < nums.size(); i++)
+//             s.insert(nums[i]);
+//         for(int i = 0; i <= nums.size(); i++)
+//         {
+//             if(s.find(i) == s.end())
+//                 return i;
+//         }
+//         return -1;
+//     }
+// };
+
+// Optimized Approach Space O(1)
 class Solution {
 public:
     int missingNumber(vector<int>& nums) {
-        unordered_set<int> s;
-        for(int i = 0; i < nums.size(); i++)
-            s.insert(nums[i]);
+        int sum = 0;
+        int actualSum = 0;
         for(int i = 0; i <= nums.size(); i++)
-        {
-            if(s.find(i) == s.end())
-                return i;
-        }
-        return -1;
+            sum += i;
+        
+        for(int i = 0; i < nums.size(); i++)
+            actualSum += nums[i];
+        
+        return sum - actualSum;
     }
 };
