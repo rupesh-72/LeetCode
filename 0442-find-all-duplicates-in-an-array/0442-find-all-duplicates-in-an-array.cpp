844074687076143e905eb77e9f5a6dd1d16e2f95
rupesh-72 <120365138+rupesh-72@users.noreply.github.com>
2023-03-28 14:46:24 +0530

    Time: 53 ms (91.41%), Space: 33.6 MB (60.86%) - LeetHub

--- 0442-find-all-duplicates-in-an-array/0442-find-all-duplicates-in-an-array.cpp
+++ 0442-find-all-duplicates-in-an-array/0442-find-all-duplicates-in-an-array.cpp
@@ -1,18 +1,38 @@
+// Time - O(nlogn), Space - O(1)
+// class Solution {
+// public:
+//     vector<int> findDuplicates(vector<int>& nums) {
+//         vector<int> res;
+//         sort(nums.begin(), nums.end());
+//         for(int i = 0; i < nums.size() - 1;)
+//         {
+//             if(nums[i] == nums[i+1])
+//             {
+//                 res.push_back(nums[i]);
+//                 i += 2;
+//             }
+//             else
+//                 i++;
+//         }
+//         return res;
+//     }
+// };
+
+// Optimized Approach Time - O(n) & Space O(1)
 class Solution {
 public:
     vector<int> findDuplicates(vector<int>& nums) {
         vector<int> res;
-        sort(nums.begin(), nums.end());
-        for(int i = 0; i < nums.size() - 1;)
+        int idx;
+        for(int i = 0; i < nums.size(); i++)
         {
-            if(nums[i] == nums[i+1])
-            {
-                res.push_back(nums[i]);
-                i += 2;
-            }
+            idx = abs(nums[i]) - 1;
+            if(nums[idx] > 0)
+                nums[idx] = -nums[idx];
             else
-                i++;
+                res.push_back(abs(nums[i]));
         }
         return res;
     }
 };
+// https://www.youtube.com/watch?v=ACYunkWQnSI
