0e9fa1ab2d76c0a1219767952d2e4b8479a864f7
rupesh-72 <120365138+rupesh-72@users.noreply.github.com>
2023-04-18 21:01:46 +0530

    Time: 0 ms (100.00%), Space: 6 MB (7.39%) - LeetHub

--- 2413-smallest-even-multiple/2413-smallest-even-multiple.cpp
+++ 2413-smallest-even-multiple/2413-smallest-even-multiple.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     int smallestEvenMultiple(int n) {
-        if(n%2 == 0)
+        if((n&1) != 1) // checking even
             return n;
         else
             return 2*n;
