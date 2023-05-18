1fea851186026eb6636a91a5a02e973387da6817
rupesh-72 <120365138+rupesh-72@users.noreply.github.com>
2023-05-18 16:07:27 +0530

    Time: 275 ms (43.74%), Space: 116.6 MB (75.14%) - LeetHub

--- 2130-maximum-twin-sum-of-a-linked-list/2130-maximum-twin-sum-of-a-linked-list.cpp
+++ 2130-maximum-twin-sum-of-a-linked-list/2130-maximum-twin-sum-of-a-linked-list.cpp
@@ -8,21 +8,60 @@
  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
  */
+
+// Bruteforce Approach
+// Time Complexity - O(n) Spcae - O(n)
+// class Solution {
+// public:
+//     int pairSum(ListNode* head) {
+//         vector<int> v;
+//         ListNode* ptr = head;
+//         while(ptr != NULL){
+//             v.push_back(ptr->val);
+//             ptr = ptr->next;
+//         }
+        
+//         int maxSum = 0;
+//         int sum = 0;
+//         for(int i = 0; i < v.size(); i++){
+//             sum = v[i] + v[v.size()-i-1];
+//             maxSum = max(maxSum, sum);
+//         }
+//         return maxSum;
+//     }
+// };
+
+// Optimized Approach
+// Time Complexity - O(n) Spcae - O(1)
 class Solution {
 public:
     int pairSum(ListNode* head) {
-        vector<int> v;
-        ListNode* ptr = head;
-        while(ptr != NULL){
-            v.push_back(ptr->val);
-            ptr = ptr->next;
+        //find middle of LinkedList
+        ListNode* slow = head;
+        ListNode* fast = head;
+        while(fast != NULL && fast->next != NULL){
+            slow = slow->next;
+            fast = fast->next->next;
         }
         
-        int maxSum = 0;
-        int sum = 0;
-        for(int i = 0; i < v.size(); i++){
-            sum = v[i] + v[v.size()-i-1];
-            maxSum = max(maxSum, sum);
+        //Reverse Linked List from Middle
+        ListNode* curr = slow;
+        ListNode* next, *prev = NULL;
+        while(curr != NULL){
+            next = curr->next;
+            curr->next = prev;
+            prev = curr;
+            curr = next;
+        }
+        
+        //finding maxSum
+        int maxSum = 0, sum = 0;
+        ListNode *ptr1 = head, *ptr2 = prev;
+        while(ptr1 != NULL && ptr2 != NULL){
+            sum = ptr1->val + ptr2->val;
+            maxSum = max(sum, maxSum);
+            ptr1 = ptr1->next;
+            ptr2 = ptr2->next;
         }
         return maxSum;
     }
